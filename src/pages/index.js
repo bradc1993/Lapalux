9998c3acc869f1b95c562be4d362ceeeb66b0b0d
bradc1993 <bradc1993@gmail.com>
2021-01-24 21:21:56 -0600

    netlify error fix attempt #3

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,74 +19,74 @@ const IndexPage = () => {
   }
 
   return (
-    <AnimatePresence>
-      <SEO title="Home" key="seo" />
-      {!entered ? (
-        <motion.div
-          key="loadscreen-wrapper"
-          style={{ position: "absolute", width: "100vw", height: "100vh" }}
-          initial={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 2.5, delay: 2 }}
-        >
-          <LoadScreen key="loadscreen-component" handleEnter={handleEnter} />
-        </motion.div>
-      ) : null}
-      <Layout key="layout">
-        {props => (
-          <>
-            <AnimatePresence exitBeforeEnter>
-              {props.about ? (
-                <motion.div
-                  className="about"
-                  key="about-wrapper"
-                  initial={{ x: "-50vw" }}
-                  animate={{ x: 0 }}
-                  exit={{ x: "-50vw" }}
-                  transition={{
-                    duration: 0.85,
-                    type: "spring",
-                    bounce: 0,
-                  }}
-                >
-                  <About key="about-component" />
-                </motion.div>
-              ) : null}
-              {props.menu ? (
-                <motion.div
-                  className="menu"
-                  key="menu-wrapper"
-                  initial={{ x: "50vw" }}
-                  animate={{ x: 0 }}
-                  exit={{ x: "50vw" }}
-                  transition={{
-                    duration: 0.85,
-                    type: "spring",
-                    bounce: 0,
-                  }}
-                >
-                  <Menu
-                    key="menu-component"
-                    // menu={props.menu}
-                    // closeMenu={props.closeMenu}
-                    handleSongChange={props.handleSongChange}
-                  />
-                </motion.div>
-              ) : null}
-            </AnimatePresence>
-            {entered ? (
-              <Scene key="scene">
-                <Clip
-                  key="clip"
-                  videoLoop={props.videoLoop}
-                  key={props.videoLoop}
-                />
-              </Scene>
-            ) : null}
-          </>
-        )}
-      </Layout>
-    </AnimatePresence>
+    <Layout key="layout">
+      {props => (
+        <AnimatePresence>
+          <SEO title="Home" key="seo" />
+          {!entered ? (
+            <motion.div
+              key="loadscreen-wrapper"
+              style={{ position: "absolute", width: "100vw", height: "100vh" }}
+              initial={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 2.5, delay: 2 }}
+            >
+              <LoadScreen
+                key="loadscreen-component"
+                handleEnter={handleEnter}
+              />
+            </motion.div>
+          ) : null}
+
+          {props.about ? (
+            <motion.div
+              className="about"
+              key="about-wrapper"
+              initial={{ x: "-50vw" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-50vw" }}
+              transition={{
+                duration: 0.85,
+                type: "spring",
+                bounce: 0,
+              }}
+            >
+              <About key="about-component" />
+            </motion.div>
+          ) : null}
+          {props.menu ? (
+            <motion.div
+              className="menu"
+              key="menu-wrapper"
+              initial={{ x: "50vw" }}
+              animate={{ x: 0 }}
+              exit={{ x: "50vw" }}
+              transition={{
+                duration: 0.85,
+                type: "spring",
+                bounce: 0,
+              }}
+            >
+              <Menu
+                key="menu-component"
+                // menu={props.menu}
+                // closeMenu={props.closeMenu}
+                handleSongChange={props.handleSongChange}
+              />
+            </motion.div>
+          ) : null}
+          {entered ? (
+            <Scene key="scene">
+              <Clip
+                key="clip"
+                videoLoop={props.videoLoop}
+                key={props.videoLoop}
+              />
+            </Scene>
+          ) : null}
+        </AnimatePresence>
+      )}
+    </Layout>
   )
 }
 
